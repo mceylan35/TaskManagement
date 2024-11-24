d03edf609558bdd77b068d9f25273cfe9bbec352
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:33:33 +0300

    add task domains

---
+++ TaskManagement.Domain/Entities/TaskDomain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Common;
+using TaskManagement.Domain.Enums;
+
+namespace TaskManagement.Domain.Entities
+{
+    public class TaskDomain : BaseEntity
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public Status Status { get; private set; }
+        public Guid AssignedUserId { get; private set; }
+        public virtual User AssignedUser { get; private set; }
+
+        private TaskDomain() { } 
+
+        public TaskDomain(string title, string description, Guid assignedUserId)
+        {
+            Title = title;
+            Description = description;
+            AssignedUserId = assignedUserId;
+            Status = Status.NotStarted;
+        }
+
+        public void UpdateStatus(Status newStatus)
+        {
+            Status = newStatus;
+            ModifiedDate = DateTime.UtcNow;
+        }
+
+        public void Update(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            ModifiedDate = DateTime.UtcNow;
+        }
+    }
+}
