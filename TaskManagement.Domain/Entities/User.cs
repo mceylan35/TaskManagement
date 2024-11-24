ce34a89554011f19307f0408ba33425d9e03fb1a
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:33:57 +0300

    add user domains

---
+++ TaskManagement.Domain/Entities/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Common;
+using TaskManagement.Domain.Enums;
+using TaskManagement.Domain.ValueObjects;
+
+namespace TaskManagement.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+        public string TCIdentityNumber { get; private set; }
+        public UserRole Role { get; private set; }
+        public ICollection<TaskDomain> Tasks { get; private set; }
+
+        private User() { } 
+
+        public User(string firstName, string lastName, string email, string tcIdentityNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            TCIdentityNumber = tcIdentityNumber;
+            Role = UserRole.User;
+            Tasks = new List<TaskDomain>();
+        }
+
+        public void SetPassword(string passwordHash)
+        {
+            this.PasswordHash = passwordHash;
+        }
+
+        public void UpdatePersonalInfo(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
---
+++ TaskManagement.Domain/ValueObjects/TCIdentityNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Exceptions;
+
+namespace TaskManagement.Domain.ValueObjects
+{
+    public class TCIdentityNumber 
+    {
+        public string Value { get; private set; }
+
+        private TCIdentityNumber() { } // For EF Core
+
+        public TCIdentityNumber(string value)
+        {
+            if (!IsValid(value))
+                throw new DomainException("Invalid TC Identity Number");
+
+            Value = value;
+        }
+
+        public static bool IsValid(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length != 11)
+                return false;
+
+            return value.All(char.IsDigit);
+        }
+
+        
+    }
+}
