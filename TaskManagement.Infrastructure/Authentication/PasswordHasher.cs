18198ec02b79148ccdf29ba2ca04e5965d089e02
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:34:16 +0300

    add password hasher

---
+++ TaskManagement.Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.Services.Authentication;
+
+namespace TaskManagement.Infrastructure.Authentication
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int WorkFactor = 12;
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
