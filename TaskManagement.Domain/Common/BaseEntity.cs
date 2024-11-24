4394ba098d9ad7bcb2b978ffc08ac745166c63e1
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:33:18 +0300

    add baseentity

---
+++ TaskManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedDate { get; protected set; }
+        public DateTime? ModifiedDate { get; protected set; }
+        public string? CreatedBy { get; protected set; }
+        public string? ModifiedBy { get; protected set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.UtcNow;
+            
+        }
+    }
+
+}
