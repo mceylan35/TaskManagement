93cdddde2fa56e2a3d7031ff804d574989d9067f
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:37:37 +0300

    add jwttokengenerator and passwordgenerator interface

---
+++ TaskManagement.Application/Services/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.Services.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(Domain.Entities.User user);
+    }
+}
