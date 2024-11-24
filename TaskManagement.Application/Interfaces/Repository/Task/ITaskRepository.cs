b217efb841ed2eb04442579b820c120677a6deca
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:36:18 +0300

    add user and task interface repositories

---
+++ TaskManagement.Application/Interfaces/Repository/Task/ITaskRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Interfaces.Repository.Task
+{
+    public interface ITaskRepository
+    {
+        Task<TaskDomain> GetByIdAsync(Guid id);
+        Task<List<TaskDomain>> GetAllAsync();
+        Task<List<TaskDomain>> GetByUserIdAsync(Guid userId);
+        Task<List<TaskDomain>> GetAllWithUsersAsync();
+        Task<TaskDomain> GetByIdWithUserAsync(Guid id);
+        Task<TaskDomain> AddAsync(TaskDomain taskDomain);
+        System.Threading.Tasks.Task UpdateAsync(TaskDomain taskDomain);
+        System.Threading.Tasks.Task DeleteAsync(Guid id);
+    }
+}
---
+++ TaskManagement.Application/Interfaces/Repository/User/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.Interfaces.Repository.User
+{
+    public interface IUserRepository
+    {
+        Task<Domain.Entities.User> GetByIdAsync(Guid id);
+      
+        Task<Domain.Entities.User> GetByEmailAsync(string email);
+        Task<Domain.Entities.User> GetByTCIdentityNumberAsync(string tcIdentityNumber);
+        Task<bool> ExistsAsync(string email);
+        Task<Domain.Entities.User> AddAsync(Domain.Entities.User user);
+       
+      
+    }
+}
