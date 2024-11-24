2eb1b75df3c07ef660db8586dce95436439a28a4
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:37:59 +0300

    add servicecollectionextension to application layer

---
+++ TaskManagement.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.Behaviors;
+using TaskManagement.Application.Interfaces.Services.Task;
+using TaskManagement.Application.Interfaces.Services.User;
+using TaskManagement.Application.Mapping;
+using TaskManagement.Application.Services.Task;
+using TaskManagement.Application.Services.User;
+
+namespace TaskManagement.Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        { 
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+             
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+             
+             
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+             
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddMaps(Assembly.GetExecutingAssembly());
+                cfg.AddProfile<TaskMappingProfile>();
+                cfg.AddProfile<UserMappingProfile>();
+            });
+
+            return services;
+        }
+    }
+}
