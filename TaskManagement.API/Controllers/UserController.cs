b5df0f340f6c9f435d65742a043423eb670e9390
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:38:32 +0300

    add usercontroller

---
+++ TaskManagement.API/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Application.DTOs;
+using TaskManagement.Application.Interfaces.Services.User;
+
+namespace TaskManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ICurrentUserService _currentUserService;
+
+        public UserController(IUserService userService, ICurrentUserService currentUserService)
+        {
+            _userService = userService;
+            _currentUserService = currentUserService;
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<ActionResult<UserDto>> Register(CreateUserDto createUserDto)
+        {
+            var user = await _userService.CreateUserAsync(createUserDto);
+            return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<TokenDto>> Login(LoginDto loginDto)
+        {
+            var token = await _userService.LoginAsync(loginDto);
+            return Ok(token);
+        }
+
+        
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetById(Guid id)
+        {
+            if (!_currentUserService.IsAdmin && id != _currentUserService.UserId)
+                return Forbid();
+
+            var user = await _userService.GetUserByIdAsync(id);
+            return Ok(user);
+        }
+
+        
+    }
+}
