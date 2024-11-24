65d38267c1ed85c8ac5cb57d4ff5a2a0583a2250
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:33:46 +0300

    add exceptions classes

---
+++ TaskManagement.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Exceptions
+{
+    public class BadRequestException : DomainException
+    {
+        public BadRequestException() : base() { }
+
+        public BadRequestException(string message) : base(message) { }
+    }
+}
---
+++ TaskManagement.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Exceptions
+{
+    public class NotFoundException : DomainException
+    {
+        public NotFoundException() : base() { }
+
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.") { }
+    }
+}
