bad4e1e2c98e206e12af1ab20e2a768085a738f9
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:37:12 +0300

    add task and user mapping profile

---
+++ TaskManagement.Application/Mapping/TaskMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.DTOs;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Mapping
+{
+    public class TaskMappingProfile : Profile
+    {
+        public TaskMappingProfile()
+        {
+            CreateMap<TaskDomain, TaskDto>() ;
+
+            CreateMap<CreateTaskDto, TaskDomain>();
+            CreateMap<UpdateTaskDto, TaskDomain>();
+        }
+    }
+
+}
---
+++ TaskManagement.Application/Mapping/UserMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.DTOs;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Mapping
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserDto>();
+
+            CreateMap<CreateUserDto, User>();
+            CreateMap<UpdateUserDto, User>();
+        }
+    }
+}
