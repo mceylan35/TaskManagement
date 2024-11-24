4e8b38bc5a7fe070832c547f7a55803125b401b0
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-24 14:34:58 +0300

    add userrepository

---
+++ TaskManagement.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.Interfaces.Repository.User;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Infrastructure.Persistence.Context;
+
+namespace TaskManagement.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TaskManagementDbContext _context;
+
+        public UserRepository(TaskManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await _context.Users
+                .Include(u => u.Tasks)
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+      
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> GetByTCIdentityNumberAsync(string tcIdentityNumber)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.TCIdentityNumber == tcIdentityNumber);
+        }
+
+        public async Task<bool> ExistsAsync(string email)
+        {
+            return await _context.Users
+                .AnyAsync(u => u.Email == email);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+       
+
+      
+    }
+}
